4123e8752cc74e459a0c8f094be36f7db26a8a90
Running-horse <3138265636@qq.com>
2025-04-02 22:02:11 +0800

    init

---
+++ train.py
@@ -0,0 +1,21 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('ultralytics/cfg/models/11/yolo11-CGAFusion-MSAM.yaml')
+    #model.load('best.pt') # loading pretrain weights
+    model.train(data='data/Rail.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=300,
+                batch=16,
+                workers=0,
+                device='0',
+                optimizer='SGD', # using SGD
+                # resume='', # last.pt path
+                # amp=False # close amp
+                # fraction=0.2,
+                project='runs/train',
+                name='exp',
+                )
